13e15c0f558d2e892ea3771335ecb523bbef0029
meet-ca <163323129+meet-ca@users.noreply.github.com>
2024-07-16 20:51:48 -0400

    Add files via upload

---
+++ eks-project/eks-deployment.groovy
@@ -0,0 +1,43 @@
+pipeline {
+    agent any
+    environment {
+        AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
+        AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
+        AWS_DEFAULT_REGION = 'us-east-1'
+    }
+    stages{
+        stage('Checkout SCM'){
+            steps{
+                script{
+                    checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/meet-ca/eks-project']])
+                }
+            }
+        }
+        stage('Initializing Terraform'){
+            steps{
+                bat 'terraform init'
+                      
+            }
+        }
+        stage('Validating Terraform'){
+            steps{
+                bat 'terraform validate'
+            }
+        }
+        stage('Previewing the infrastructure'){
+            steps{
+                bat 'terraform plan'
+                input(message: "Approve?", ok: "proceed")
+            }
+        }
+        stage('Create/Destroy an EKS cluster'){
+            steps{
+                script{
+                    {
+                         bat 'terraform $action --auto-approve'
+                    }
+                }
+            }
+        }
+    }
+}
